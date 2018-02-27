c75b03bf2dcc870ee901f647a026b1d71ca724fc
Pedro Belli <pedro.belli2@gmail.com>
2018-02-26 22:12:45 -0300

    Mod 3 - Aula 33.

--- catalog/views.py
+++ catalog/views.py
@@ -1,21 +1,31 @@
 # coding=utf-8
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views import generic
+
 from .models import Product, Category
 
-def product_list(request):
-    context = {
-        'products': Product.objects.all()
-    }
-    return render(request, 'catalog/product_list.html', context)
+class ProductListView(generic.ListView):
 
-def category(request, slug):
-    category = Category.objects.get(slug=slug)
-    context = {
-        'current_category': category,
-        'products': Product.objects.filter(category=category)
-    }
-    return render(request, 'catalog/category.html', context)
+    model = Product
+    context_object_name = 'products'
+    template_name = 'catalog/product_list.html'
+
+class CategoryListView(generic.ListView):
+
+    context_object_name = 'products'
+    template_name = 'catalog/category.html'
+
+    def get_queryset(self):
+        return Product.objects.filter(category__slug=self.kwargs['slug'])
+
+    def get_context_data(self, **kwargs):
+        context = super(CategoryListView, self).get_context_data(**kwargs)
+        context['current_category'] = get_object_or_404(Category, slug=self.kwargs['slug'])
+        return context
+
+product_list = ProductListView.as_view()
+category = CategoryListView.as_view()
 
 def product(request, slug):
     product = Product.objects.get(slug=slug)
